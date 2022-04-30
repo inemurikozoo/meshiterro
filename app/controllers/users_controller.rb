df191dba1275e7d2c1f3cd8848a65be1ac4d985e
inemurikozoo <100814669+inemurikozoo@users.noreply.github.com>
2022-04-30 01:12:31 +0000

    meshiterro chapter16 studing.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,25 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    #特定のユーザ@userに関連付けられたすべて.post_imagesを取得、@post_imagesに渡す
+    @post_images = @user.post_images
+    
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+  
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name,:profile_image)
   end
 end
